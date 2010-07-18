1acc0480268dff8021c0af0bba6850c9198a2af8
fabiantheblind <fabiantheblind@googlemail.com>
2010-07-18 16:55:25 +0200

    sorts all elements in group element by priority

--- importXML.js
+++ importXML.js
@@ -16,10 +16,11 @@ main();
 
 function main(){
 	
-	
+	var numOfItems;
 	var myOS_is_Windows;
 	try{
 	 myOS_is_Windows = checkOS();
+	 
 	} catch (e){
 		alert("ERROR: I dont know wich Operating System you are using\n"+ e );
 		exit();
@@ -31,39 +32,67 @@ function main(){
 		alert("ERROR: you have no active Document!\n" + e);
 		exit();
 }
-// set the XMLImport preferences
-	xmlImportPref(myDoc);
 
-	var myRoot;
-	try {
-		myRoot = myDoc.importXML(File.openDialog("Choose your .xml file"));
-	} catch (e) {
-		alert("ERROR: :( Sorry, your XML Document seems broken.\n" + e);
+	// set the XMLImport preferences
+		xmlImportPref(myDoc);
+
+		var myRoot;
+		try {
+			myRoot = myDoc.importXML(File.openDialog("Choose your .xml file"));
+		} catch (e) {
+			alert("ERROR: :( Sorry, your XML Document seems broken.\n" + e);
+			exit();
+		}
+
+		try{
+			makeAttributesFromInfo(myDoc);
+			
+		}catch (e){
+		alert("ERROR: No i could not make your Attributes for processing the xml" + e);
 		exit();
-	}
-	
- try{
-	makeAttributesFromInfo(myDoc);
-	
- }catch (e){
- alert("No i could not make your Attributes for processing the xml" + e);
- exit();
- }
- 
-	
-	
+		}
 
-	
-	makeGroupElements(myDoc);
-	
-	for (var i = 1 ; i < 11 ;i++ ){
- 	sortToGroupsByAttributes(myDoc,i);
-	}
+		try{
+			makeGroupElements(myDoc);
+		}catch(e){
+			
+			alert("ERROR: Could not make the Group elements" + e);
+			exit();
+		}
+		
+		try{
+			for (var i = 1 ; i < 11 ;i++ ){
+				sortToGroupsByAttributes(myDoc,i);
+				}	
+		}catch(e){
+			alert("ERROR: Could not move Elements to group" + e);
+			exit();
+		}
+		try{
+// first we move the normal ones to the front
+		sortInGroupByPriorityNormal(myDoc);
+		}catch(e){
+			
+			alert("ERROR: Could not move the normal Elements in group" + e);
+			exit();	
+		}
+		
+		try{
+// than we move the fokus to the front
+			sortInGroupByPriorityFocus(myDoc);
+			}catch(e){
+				
+				alert("ERROR: Could not move the fokus Elements in group" + e);
+				exit();	
+			}
+			
+// the klein artikel stay at the end
 
-	
-	
+		
+
+
 // -------------------------
-// 	From here on it does something on the page
+// From here on it does something on the page
 	var myPageName;
 	var myPage;
 	try{
@@ -78,105 +107,6 @@ function main(){
 	myFrame.geometricBounds = myGetColumns(myDoc,myPage);
 
 	alert("Done");
-
-}
-
-/**
- * this makes attributes from the element artikelInformation
- * @param myDoc
- * @returns nothing
- */
-function makeAttributesFromInfo(myDoc){
-
-	var myRuleSet = new Array(new findInfoElement());
-	with(myDoc){
-	var elements = xmlElements;
-	__processRuleSet(elements.everyItem(), myRuleSet);
-
-	}
-}
-	
-/**
- * this is the RuleSet for makeAttributesFromInfo
- * @returns nothing
- */
-function findInfoElement(){
-	this.name = "findInfoElement";
-	this.xpath = "/Root/seite/artikel/artikelInformation";
-	this.apply = function(myElement, myRuleProcessor){
-		var myItem;
-			for(var i = 0; i < myElement.xmlElements.length; i++){
-				
-			myItem= myElement.xmlElements.item(i);
-			
-			myElement.parent.xmlAttributes.add(myItem.markupTag.name, myItem.texts.item(0).contents);	
-			}
-		}
-
-}
-
-
-/**
- * reorganizes the xmlStructure in element <seite> to groups
- * @param myDoc
- * @param count
- * @returns
- */
-function sortToGroupsByAttributes(myDoc,count){
-
-	var myRuleSet = new Array(new findGroupAttribute(count));
-	with(myDoc){
-	var elements = xmlElements;
-	__processRuleSet(elements.everyItem(), myRuleSet);
-
-	}
-}
-
-/**
- * moves the grouped items into a new xmlElement
- * needed for sortToGroupsByAttributes(myDoc,count)
- * needs the for loop to process
- * @param count
- * @returns
- */
-function findGroupAttribute(count){
-
-	this.name = "findGroupAttribute";
-	this.xpath = "/Root/seite/artikel[@iGruppenFarbe ='"+count+". Gruppenfarbe']";
-	this.apply = function(myElement, myRuleProcessor){
-		__skipChildren(myRuleProcessor);
-		myElement.move(LocationOptions.UNKNOWN,myElement.parent.xmlElements.item(count));
-		}
-	
-}
-
-/**
- * this makes new elements for grouping all the <artikel> elements
- * @param myDoc
- * @returns
- */
-function makeGroupElements(myDoc){
-	var myRuleSet = new Array(new findPage());
-	with(myDoc){
-	var elements = xmlElements;
-	__processRuleSet(elements.everyItem(), myRuleSet);
-	}
-}
-/**
- * this is the function for makeGroupElements
- * @returns
- */
-function findPage(){
-	this.name = "findPage";
-	this.xpath = "/Root/seite";
-	this.apply = function(myElement, myRuleProcessor){
-		
-			for(var i = 10; i >= 0; i--){
-			var myNewGroupElement = myElement.xmlElements.add("group");
-			myNewGroupElement.xmlAttributes.add("id",i.toString());
-			myNewGroupElement.move(LocationOptions.AT_BEGINNING,myElement);
-			}
-		}
 
 }
 
@@ -241,6 +171,9 @@ function checkOS(){
 	}
 }
 
+function processXML(myDoc){
+}
+
 
 /**
  * Set the xml import preferences
@@ -251,13 +184,169 @@ function xmlImportPref(myDoc){
 	myXMLImportPreferences.allowTransform = false;
 	myXMLImportPreferences.createLinkToXML = false;
 	myXMLImportPreferences.ignoreUnmatchedIncoming = false;
-	myXMLImportPreferences.ignoreWhitespace = true;
+	myXMLImportPreferences.ignoreWhitespace = false;
 	myXMLImportPreferences.importCALSTables = false;
 	myXMLImportPreferences.importStyle = XMLImportStyles.mergeImport;
 	myXMLImportPreferences.importTextIntoTables = false;
 	myXMLImportPreferences.importToSelected = false;
 	myXMLImportPreferences.removeUnmatchedExisting = false;
 	myXMLImportPreferences.repeatTextElements = false;
+}
+
+
+/**
+ * this makes attributes from the element artikelInformation
+ * 
+ * @param myDoc
+ * @returns nothing
+ */
+function makeAttributesFromInfo(myDoc){
+
+	var myRuleSet = new Array(new findInfoElement());
+	with(myDoc){
+	var elements = xmlElements;
+	__processRuleSet(elements.everyItem(), myRuleSet);
+
+	}
+}
+	
+/**
+ * this is the RuleSet for makeAttributesFromInfo
+ * 
+ * @returns nothing
+ */
+function findInfoElement(){
+	this.name = "findInfoElement";
+	this.xpath = "/Root/seite/artikel/artikelInformation";
+	this.apply = function(myElement, myRuleProcessor){
+		var myItem;
+			for(var i = 0; i < myElement.xmlElements.length; i++){
+				
+			myItem= myElement.xmlElements.item(i);
+			
+			myElement.parent.xmlAttributes.add(myItem.markupTag.name, myItem.texts.item(0).contents);	
+			}
+		}
+
+}
+/**
+ * 
+ * @param myDoc
+ * @returns nothing
+ */
+function sortInGroupByPriorityFocus(myDoc){
+
+	var myRuleSet = new Array(new sortFocus());
+	with(myDoc){
+	var elements = xmlElements;
+	__processRuleSet(elements.everyItem(), myRuleSet);
+
+	}
+	
+
+}
+	
+/**
+ * 
+ * @returns nothing
+ */
+function sortFocus(){
+	this.name = "sortFocus";
+	this.xpath = "/Root/seite/group/artikel[@iPrioritaet ='fokus']";
+	this.apply = function(myElement, myRuleProcessor){
+		__skipChildren(myRuleProcessor);
+		myElement.move(LocationOptions.before,myElement.parent.xmlElements.item(0));
+		}
+
+}
+/**
+ * 
+ * @param myDoc
+ * @returns nothing
+ */
+function sortInGroupByPriorityNormal(myDoc){	
+	var myRuleSet = new Array(new sortNormal());
+	with(myDoc){
+	var elements = xmlElements;
+	__processRuleSet(elements.everyItem(), myRuleSet);
+
+	}
+}
+function sortNormal(){
+	this.name = "sortNormal";
+	this.xpath = "/Root/seite/group/artikel[@iPrioritaet ='normal']";
+	this.apply = function(myElement, myRuleProcessor){
+		__skipChildren(myRuleProcessor);
+		myElement.move(LocationOptions.before,myElement.parent.xmlElements.item(0));
+		}
+
+}
+
+/**
+ * reorganizes the xmlStructure in element <seite> to groups
+ * 
+ * @param myDoc
+ * @param count
+ * @returns
+ */
+function sortToGroupsByAttributes(myDoc,count){
+
+	var myRuleSet = new Array(new findGroupAttribute(count));
+	with(myDoc){
+	var elements = xmlElements;
+	__processRuleSet(elements.everyItem(), myRuleSet);
+
+	}
+}
+
+/**
+ * moves the grouped items into a new xmlElement needed for
+ * sortToGroupsByAttributes(myDoc,count) needs the for loop to process
+ * 
+ * @param count
+ * @returns
+ */
+function findGroupAttribute(count){
+
+	this.name = "findGroupAttribute";
+	this.xpath = "/Root/seite/artikel[@iGruppenFarbe ='"+count+". Gruppenfarbe']";
+	this.apply = function(myElement, myRuleProcessor){
+		__skipChildren(myRuleProcessor);
+		myElement.move(LocationOptions.UNKNOWN,myElement.parent.xmlElements.item(count));
+		}
+	
+}
+
+/**
+ * this makes new elements for grouping all the <artikel> elements
+ * 
+ * @param myDoc
+ * @returns
+ */
+function makeGroupElements(myDoc){
+	var myRuleSet = new Array(new findPage());
+	with(myDoc){
+	var elements = xmlElements;
+	__processRuleSet(elements.everyItem(), myRuleSet);
+	}
+}
+/**
+ * this is the function for makeGroupElements
+ * 
+ * @returns
+ */
+function findPage(){
+	this.name = "findPage";
+	this.xpath = "/Root/seite";
+	this.apply = function(myElement, myRuleProcessor){
+		
+			for(var i = 10; i >= 0; i--){
+			var myNewGroupElement = myElement.xmlElements.add("group");
+			myNewGroupElement.xmlAttributes.add("id",i.toString());
+			myNewGroupElement.move(LocationOptions.AT_BEGINNING,myElement);
+			}
+		}
+
 }
 
 /**
