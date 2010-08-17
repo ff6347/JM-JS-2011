044e7ea9a7017ee022c4c5c26d253689d0b451d3
fabiantheblind <fabiantheblind@googlemail.com>
2010-08-17 11:36:49 +0200

    Make Buttons runs. needs paragraph style

--- makeLinkButton.jsx
+++ makeLinkButton.jsx
@@ -11,6 +11,7 @@ function main() {
 	var myList;
 	var myPageName;
 	var myPage;
+	myDoc.paragraphStyles.item("ERROR").appliedFont = "Arial";
 
 	myList = myDoc.pages.everyItem().name;
 
@@ -87,39 +88,13 @@ function makeButton(theItem, myPage, myDoc){
 
 		
 		
-		var myRect01 = myPage.rectangles.add();
-		with (myRect01) {
-			geometricBounds = [myY1, myX1, myY2, myX2];
-			applyObjectStyle(myNullObjStyle);
-		}
-		myGroup.push(myRect01);
-		
-
-		
-		
-		
-		var myTF01 = myPage.textFrames.add();
-		with (myTF01) {
-			geometricBounds = [myY1, myX1, myY2, myX2];
-			applyObjectStyle(myNullObjStyle);
-			
-		}
-		var myContentString = "Dazu passt:"+"\n"+
-		myElement.xmlElements.item("artikelInformation").xmlElements.item("iHersteller").texts.everyItem()+"\n"+
-		myElement.xmlElements.item("artikelInformation").xmlElements.item("iArtikelBezeichnung").texts.everyItem()+"\n"+
-		myElement.xmlElements.item("artikelInformation").xmlElements.item("iArtikelBeschreibung").texts.everyItem()+"\n"+
-		myElement.xmlElements.item("artikelInformation").xmlElements.item("iArtikelNr").texts.everyItem()+"\n"+
-		"Dies und mehr findet Ihr auf Seite XXX";
-
-		myTF01.fit(FitOptions.FRAME_TO_CONTENT);
-
-		myGroup.push(myTF01);
-		
-		
-		
+		var OY1 = 15;//myY1 + 7.5;
+		var OX1 = 15;//myX1 + 7.5 ;
+		var OY2 = OY1 + 15;
+		var OX2 = OX1 + 15;
 		var myOV01 = myPage.ovals.add();
 		with (myOV01) {
-			geometricBounds = [myY1, myX1, myY2, myX2];
+			geometricBounds = [OY1, OX1 ,OY2 , OX2];
 			applyObjectStyle(myNullObjStyle);	
 		}
 
@@ -133,17 +108,75 @@ function makeButton(theItem, myPage, myDoc){
 		myGroup.push(myOV01);
 		
 		
+		
+		var myRect01 = myPage.rectangles.add();
+		with (myRect01) {
+			geometricBounds = [myY1, myX1, myY2, myX2];
+			applyObjectStyle(myNullObjStyle);
+		}
+		var myFile  = File.openDialog("Choose the File \"Button_Lupe_Hintergrund.bmp\"");
+		myRect01.place(myFile);
+		myRect01.fit(FitOptions.CENTER_CONTENT);
+		myRect01.fit(FitOptions.PROPORTIONALLY);
+		myRect01.fit(FitOptions.FRAME_TO_CONTENT);
+
+		myGroup.push(myRect01);
+		
+
+		
+		
+		
+		var TY1 = myY1 +7;
+		var TX1 = myX1 + 21;
+		var TY2 = TY1 + 50;
+		var TX2 =  TX1 + 50;
+		
+		
+		
+		var myTF01 = myPage.textFrames.add();
+		with (myTF01) {
+			geometricBounds = [TY1, TX1, TY2, TX2];
+			applyObjectStyle(myNullObjStyle);
+			
+		}
+		
+		//var myContentElement = myElement.xmlElements.add("buttontext");
+		
+		//myContentElement.insertTextAsContent("Dazu passt:\n", XMLElementPosition.elementStart);
+		var myContentString = "Dazu passt:"+"\n"+
+		myElement.xmlElements.item("artikelInformation").xmlElements.item("iHersteller").contents +"\r"+
+		myElement.xmlElements.item("artikelInformation").xmlElements.item("iArtikelBezeichnung").contents +"\n"+
+		myElement.xmlElements.item("artikelInformation").xmlElements.item("iArtikelBeschreibung").contents +"\n"+
+		myElement.xmlElements.item("artikelInformation").xmlElements.item("iArtikelNr").contents +"\n"+
+		"Dies und mehr findet Ihr auf Seite XXX";
+
+		myTF01.contents = myContentString;
+		myTF01.paragraphs.everyItem().appliedParagraphStyle = myDoc.paragraphStyles.item("ERROR");
+		myTF01.characters.everyItem().appliedCharacterStyle = myDoc.characterStyles.item(0);
+		
+		myTF01.texts.everyItem().clearOverrides(OverrideType.all);
+		myTF01.fit(FitOptions.FRAME_TO_CONTENT);
+		myGroup.push(myTF01);
+		
+		
+		
+		
 		var myNewPriceElement = myElement.xmlElements.item("preis").duplicate();
+		
+		var PY1 = myY1 +15;
+		var PX1 = myX1 ;
+		var PY2 = PY1 + 50;
+		var PX2 =  PX1 + 50;
 		
 		var myPF01 = myPage.textFrames.add();
 		with (myPF01) {
-			geometricBounds = [myY1, myX1, myY2, myX2];
+			geometricBounds = [PY1, PX1, PY2, PX2];
 			applyObjectStyle(myNullObjStyle);
 			
 		}
 		myNewPriceElement.placeXML(myPF01);
-		//myNewPriceElement.untag();
-		myPF01.paragraphs.item(0).appliedParagraphStyle = myDoc.paragraphStyles.item("PREIS");
+		myNewPriceElement.untag();
+		myPF01.paragraphs.item(0).appliedParagraphStyle = myDoc.paragraphStyles.item("PREIS_KLEIN");
 		myPF01.fit(FitOptions.FRAME_TO_CONTENT);
 
 		
