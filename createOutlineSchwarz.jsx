92bbb47ac865db155d191b09b48ef847fc3a58d8
fabiantheblind <fabiantheblind@googlemail.com>
2010-09-19 19:55:45 +0200

    now it is really black

--- createOutlineSchwarz.jsx
+++ createOutlineSchwarz.jsx
@@ -1,5 +1,4 @@
 //changing the color for outlines
-var myColor = app.activeDocument.colors.add({name:"Outline_Schwarz"});
-myColor = [0,0,0,100];
+var myColor = app.activeDocument.colors.add({name:"Outline_Schwarz",model:ColorModel.process, colorValue:[0, 0, 0, 100]});
 
 
